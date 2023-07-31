83e8c6a8986d8c66d046e05462f8370c6868c47d
valonmulolli <valonmulolli@gmail.com>
2023-07-31 16:00:31 +0200

    Post component

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,10 +1,89 @@
-import { Text } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 import { Post } from '../types';
+import { FontAwesome } from '@expo/vector-icons';
 
 type PostListItemProps = {
 	post: Post;
 };
 
-export default function PostListItem({ post }: PostListItemProps) {
-	return <Text>{post.content}</Text>;
+type FooterButtonProp = {
+	text: string;
+	icon: React.ComponentProps<typeof FontAwesome>['name'];
+};
+function FooterButton({ text, icon }: FooterButtonProp) {
+	return (
+		<View style={{ flexDirection: 'row' }}>
+			<FontAwesome name={icon} size={24} color='black' />
+			<Text style={{ marginLeft: 5, color: 'gray', fontWeight: '500' }}>
+				{text}
+			</Text>
+		</View>
+	);
 }
+
+export default function PostListItem({ post }: PostListItemProps) {
+	return (
+		<View style={styles.container}>
+			{/* Header */}
+			<View style={styles.header}>
+				<Image style={styles.userImage} source={{ uri: post.author.image }} />
+				<View>
+					<Text style={styles.userName}>{post.author.name}</Text>
+					<Text>{post.author.position}</Text>
+				</View>
+			</View>
+
+			{/* Text content */}
+			<Text style={styles.content}>{post.content}</Text>
+
+			{/* Image content */}
+			{post.image && (
+				<Image source={{ uri: post.image }} style={styles.postImage} />
+			)}
+
+			{/* Footer */}
+			<View style={styles.footer}>
+				<FooterButton text='Likes' icon='thumbs-o-up' />
+				<FooterButton text='Comments' icon='comment-o' />
+				<FooterButton text='Share' icon='share' />
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: 'white',
+	},
+	header: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		padding: 10,
+	},
+	userName: {
+		fontWeight: 'bold',
+		fontSize: 16,
+		marginBottom: 5,
+	},
+	userImage: {
+		width: 50,
+		height: 50,
+		borderRadius: 25,
+		marginRight: 10,
+	},
+	content: {
+		margin: 10,
+		marginTop: 10,
+	},
+	postImage: {
+		width: '100%',
+		aspectRatio: 1,
+	},
+	footer: {
+		flexDirection: 'row',
+		paddingVertical: 10,
+		justifyContent: 'space-around',
+		borderTopWidth: 0.5,
+		borderColor: 'lightgray',
+	},
+});
