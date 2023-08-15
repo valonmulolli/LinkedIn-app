34c862ce4c7500a90ea9125674ade0674a9a5f70
valonmulolli <valonmulolli@gmail.com>
2023-08-16 01:32:23 +0200

    post Paginated List

--- src/apollo/Client.tsx
+++ src/apollo/Client.tsx
@@ -1,9 +1,22 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider,TypePolicies } from '@apollo/client';
+
+const typePolicies: TypePolicies = {
+  Query: {
+    fields: {
+      postPaginatedList: {
+        keyArgs: false,
+        merge(existing = [], incoming) {
+          return [...existing, ...incoming]
+        }
+      }
+    }
+  }
+}
 
 const client = new ApolloClient({
   uri: 'https://xinhui.stepzen.net/api/linkedIn/__graphql',
   headers: {'Authorization':'apikey xinhui::stepzen.io+1000::916090f09160885a22eace8f5d1111fd71f7f5bb6b6ede68a1184225e9a795e2'},
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({ typePolicies}),
 });
 
 export default client;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { FlatList, ActivityIndicator, Text } from 'react-native';
 import PostListItem from '@/components/PostListItem';
 // import posts from '../../../assets/data/posts.json';
 import { gql, useQuery } from '@apollo/client';
+import { useState } from 'react';
 
 const postList = gql`
 	query PostListQuery {
@@ -19,8 +20,41 @@ const postList = gql`
 	}
 `;
 
+const postPaginatedList = gql`
+	query postPaginatedListQuery($first: Int, $after: Int) {
+		postPaginatedList(first: $first, after: $after) {
+			id
+			content
+			image
+			profile {
+				id
+				name
+				position
+				image
+			}
+		}
+	}
+`;
+
 export default function HomeFeedScreen() {
-	const { loading, error, data } = useQuery(postList);
+	const [hasMore, sethasMore] = useState(true);
+	const { loading, error, data, fetchMore } = useQuery(postPaginatedList, {
+		variables: { first: 2 },
+	});
+
+	const loadMore = async () => {
+		if (!hasMore) {
+			return;
+		}
+
+		const res = await fetchMore({
+			variables: { after: data.postPaginatedList.length },
+		});
+		if (res.data.postPaginatedList.length === 0) {
+			sethasMore(false);
+		}
+		console.log(res.data.postPaginatedList);
+	};
 
 	if (loading) {
 		return <ActivityIndicator />;
@@ -33,10 +67,11 @@ export default function HomeFeedScreen() {
 
 	return (
 		<FlatList
-			data={data.postList}
+			data={data.postPaginatedList}
 			renderItem={({ item }) => <PostListItem post={item} />}
 			showsVerticalScrollIndicator={false}
 			contentContainerStyle={{ gap: 10 }}
+			onEndReached={loadMore}
 		/>
 	);
 }
